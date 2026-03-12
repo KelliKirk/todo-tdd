339699efa642319711c49171862959043670f9fe
Kelli Kirk <kelli.kirk@voco.ee>
2026-03-12 14:33:44 +0200

    Create TodoController.getTodos unit test and integration tests

--- tests/integration/todo.controller.int.test.js
+++ tests/integration/todo.controller.int.test.js
@@ -3,8 +3,10 @@ const app = require("../../app")
 const newTodo = require("../mock-data/new-todo.json")
 const TodoModel = require("../../models/todo.model")
 const endpointUrl = "/todos/"
+const allTodos = require("../mock-data/all-todos.json")
 
 TodoModel.create = jest.fn()
+TodoModel.find = jest.fn()
 
 describe(endpointUrl, () => {
     it("POST " + endpointUrl, async () => {
@@ -16,6 +18,14 @@ describe(endpointUrl, () => {
         expect(response.body.title).toBe(newTodo.title)
         expect(response.body.done).toBe(newTodo.done)
     })
+    it("GET " + endpointUrl, async () => {
+    TodoModel.find.mockResolvedValue(allTodos)
+    const response = await request(app).get(endpointUrl)
+    expect(response.statusCode).toBe(200)
+    expect(Array.isArray(response.body)).toBeTruthy()
+    expect(response.body[0].title).toBeDefined()
+    expect(response.body[0].done).toBeDefined()
+})
     it("should return error 500 on malformed data with POST" + endpointUrl, async () => {
         TodoModel.create.mockRejectedValue(new Error("Todo validation failed: done: Path 'done' is required"))
         const response = await request(app)
